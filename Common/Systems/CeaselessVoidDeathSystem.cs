fc467d5b209045cb9bf4fdf620ae64a2ec137386
Big E <evanmorrison42@gmail.com>
2025-04-19 15:13:25 -0400

    + Ceaseless Void Death Animation + Music Effects

---
+++ Common/Systems/CeaselessVoidDeathSystem.cs
@@ -0,0 +1,98 @@
+﻿using CalamityMod.NPCs.CeaselessVoid;
+using Microsoft.Xna.Framework;
+using MonoStereo.Filters;
+using MonoStereoMod;
+using System.Linq;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace Sundowner.Common.Systems
+{
+    public class CeaselessVoidDeathSystem : ModSystem
+    {
+        public static SpeedChangeFilter speedUp { get; private set; } = new();
+        private static bool CeaselessVoidAlive = false;
+        private static bool SlowDown = false;
+        private static int VoidIndex = -1;
+
+        public override void UpdateUI(GameTime gameTime)
+        {
+            int voidID = NPC.FindFirstNPC(ModContent.NPCType<CeaselessVoid>());
+            if (voidID == -1)
+                return;
+            if (Main.npc[voidID].ModNPC is not CeaselessVoid)
+                return;
+
+            MonoStereoAudioTrack CurrentMusic = MonoStereoMod.MonoStereoMod.GetSong(Main.curMusic);
+            if(CurrentMusic == null)
+                return;
+            if (!CurrentMusic.Filters.Any())
+                CurrentMusic.AddFilter(speedUp);
+        }
+
+        public override void PostUpdateNPCs()
+        {
+            if (ModCompat.InfernumMode != null)
+                return;
+
+            MonoStereoAudioTrack currentMusic;
+            bool veryDead = false;
+
+            if (VoidIndex == -1)
+            {
+                int voidID = NPC.FindFirstNPC(ModContent.NPCType<CeaselessVoid>());
+                if (voidID == -1)
+                {
+                    if (CeaselessVoidAlive)
+                    {
+                        currentMusic = MonoStereoMod.MonoStereoMod.GetSong(Main.curMusic);
+                        currentMusic?.Stop();
+
+                        speedUp.Speed = 1f;
+                        CeaselessVoidAlive = false;
+                    }
+                    veryDead = true;
+                }
+                else
+                    VoidIndex = voidID;
+            }
+
+            if (!veryDead && Main.npc[VoidIndex].life <= 0)
+                veryDead = true;
+
+            if (veryDead)
+            {
+                if (SlowDown && speedUp.Speed > 0)
+                {
+                    speedUp.Speed *= 0.975f;
+                    if (speedUp.Speed < 0.001f)
+                        speedUp.Speed = 0;
+                }
+                return;
+            }
+
+            CeaselessVoidAlive = true;
+
+            if (((CeaselessVoid)Main.npc[VoidIndex].ModNPC).playedbuildsound)
+            {
+                currentMusic = MonoStereoMod.MonoStereoMod.GetSong(Main.curMusic);
+                if (currentMusic != null && !currentMusic.Filters.Any(f => f is SpeedChangeFilter))
+                    currentMusic.AddFilter(speedUp);
+
+                speedUp.Speed += 0.004f;
+            }
+            else
+            {
+                if(speedUp.Speed != 1f)
+                    speedUp.Speed = 1f;
+            }
+
+        }
+
+        public static void OnCeaselessDeath()
+        {
+            CeaselessVoidAlive = false;
+            SlowDown = true;
+        }
+    }
+}
---
+++ Content/NPCs/SundownerGlobalNPC.cs
@@ -0,0 +1,96 @@
+﻿using CalamityMod;
+using CalamityMod.NPCs.CeaselessVoid;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sundowner.Common.Systems;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ModLoader;
+
+namespace Sundowner.Content.NPCs
+{
+    public class SundownerGlobalNPC : GlobalNPC
+    {
+        public override void HitEffect(NPC npc, NPC.HitInfo hit)
+        {
+            if (npc.type == ModContent.NPCType<CeaselessVoid>() && npc.life <= 0)
+                CeaselessVoidDeathSystem.OnCeaselessDeath();
+        }
+
+        public override bool PreDraw(NPC npc, SpriteBatch spriteBatch, Vector2 screenPos, Color drawColor)
+        {
+            if (npc.type != ModContent.NPCType<CeaselessVoid>() || ModCompat.InfernumMode != null)
+                return base.PreDraw(npc, spriteBatch, screenPos, drawColor);
+
+            // Draw Code for Ceaseless Void gotten directly from Calamity, modified for the death animation
+
+            SpriteEffects spriteEffects = SpriteEffects.None;
+            if (npc.spriteDirection == 1)
+                spriteEffects = SpriteEffects.FlipHorizontally;
+
+            Texture2D texture2D15 = TextureAssets.Npc[npc.type].Value;
+            Vector2 halfSizeTexture = new((TextureAssets.Npc[npc.type].Value.Width / 2), (TextureAssets.Npc[npc.type].Value.Height / Main.npcFrameCount[npc.type] / 2));
+            
+            float rotation = npc.rotation;
+            Vector2 deathOffset = Vector2.Zero;
+
+            if (((CeaselessVoid)npc.ModNPC).playedbuildsound)
+            {
+                float currentSpeed = CeaselessVoidDeathSystem.speedUp.Speed - 1;
+                rotation += Main.rand.NextFloat(-currentSpeed, currentSpeed) / 4f;
+                deathOffset = Main.rand.NextVector2CircularEdge(currentSpeed, currentSpeed) * 4f;
+            }
+
+            int afterimageAmt = 7;
+
+            if (CalamityConfig.Instance.Afterimages)
+            {
+                for (int i = 1; i < afterimageAmt; i += 2)
+                {
+                    Color afterimageColor = drawColor;
+                    afterimageColor = Color.Lerp(afterimageColor, Color.White, 0.5f);
+                    afterimageColor = npc.GetAlpha(afterimageColor);
+                    afterimageColor *= (float)(afterimageAmt - i) / 15f;
+                    
+                    Vector2 afterimageDrawPos = npc.oldPos[i] + new Vector2((float)npc.width, (float)npc.height) / 2f - screenPos;
+                    afterimageDrawPos -= new Vector2((float)texture2D15.Width, (float)(texture2D15.Height / Main.npcFrameCount[npc.type])) * npc.scale / 2f;
+                    afterimageDrawPos += halfSizeTexture * npc.scale + new Vector2(0f, npc.gfxOffY);
+                    afterimageDrawPos += deathOffset;
+
+                    spriteBatch.Draw(texture2D15, afterimageDrawPos, npc.frame, afterimageColor, rotation, halfSizeTexture, npc.scale, spriteEffects, 0f);
+                }
+            }
+
+            Vector2 drawLocation = npc.Center - screenPos;
+            drawLocation -= new Vector2((float)texture2D15.Width, (float)(texture2D15.Height / Main.npcFrameCount[npc.type])) * npc.scale / 2f;
+            drawLocation += halfSizeTexture * npc.scale + new Vector2(0f, npc.gfxOffY);
+            drawLocation += deathOffset;
+
+            spriteBatch.Draw(texture2D15, drawLocation, npc.frame, npc.GetAlpha(drawColor), rotation, halfSizeTexture, npc.scale, spriteEffects, 0f);
+
+            texture2D15 = CeaselessVoid.GlowTexture.Value;
+            Color cyanLerp = Color.Lerp(Color.White, Color.Cyan, 0.5f);
+
+            if (CalamityConfig.Instance.Afterimages)
+            {
+                for (int j = 1; j < afterimageAmt; j++)
+                {
+                    Color extraAfterimageColor = cyanLerp;
+                    extraAfterimageColor = Color.Lerp(extraAfterimageColor, Color.White, 0.5f);
+                    extraAfterimageColor *= (float)(afterimageAmt - j) / 15f;
+                    
+                    Vector2 extraAfterimageDrawPos = npc.oldPos[j] + new Vector2((float)npc.width, (float)npc.height) / 2f - screenPos;
+                    extraAfterimageDrawPos -= new Vector2((float)texture2D15.Width, (float)(texture2D15.Height / Main.npcFrameCount[npc.type])) * npc.scale / 2f;
+                    extraAfterimageDrawPos += halfSizeTexture * npc.scale + new Vector2(0f, npc.gfxOffY);
+                    extraAfterimageDrawPos += deathOffset;
+
+                    spriteBatch.Draw(texture2D15, extraAfterimageDrawPos, npc.frame, extraAfterimageColor, rotation, halfSizeTexture, npc.scale, spriteEffects, 0f);
+                }
+            }
+
+            spriteBatch.Draw(texture2D15, drawLocation, npc.frame, cyanLerp, rotation, halfSizeTexture, npc.scale, spriteEffects, 0f);
+
+            return false;
+        }
+    }
+}
